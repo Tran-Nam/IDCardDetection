82651de1e2ee12f0bf1575f4e3a0577c9a38fa57
Tran-Nam <phuongnam9613@gmail.com>
2020-04-21 23:23:07 +0700

    convert decart coordinate to image coordinate to refine ANGLE of vector

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -6,24 +6,28 @@ def draw_paf(paf):
     h, w = paf.shape[:2]
     x = paf[:, :, 0]
     y = paf[:, :, 1]
+    y = -y # decart coodinate to image coordinate
+    #print(x)
+    #print(y)
     a = np.arange(w)
     b = np.arange(h)
     u, v = np.meshgrid(a, b)
     m = np.hypot(x, y)
-    fig, ax = plt.subplots()
-    q = ax.quiver(u, v, a, b, m)
+    fig, ax = plt.subplots(dpi=500)
+    q = ax.quiver(u, v, x, y, m, pivot='mid', scale_units='xy', scale=1)
     plt.xlim(-1, w)
     plt.ylim(h, -1)
     plt.axis('off')
-    return fig 
+    #plt.show()
+    return fig
 
 def fig2im(fig):
     fig.canvas.draw()
     w, h = fig.canvas.get_width_height()
     buf = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8)
-    buf.shape(w, h, 3)
+    buf.shape = (w, h, 3)
     buf = np.roll(buf, 3, axis=2)
-    im = Image.frombytes('RGB', (w, h), bug.tostring())
+    im = Image.frombytes('RGB', (w, h), buf.tostring())
     return im
 
 def paf2im(paf):
